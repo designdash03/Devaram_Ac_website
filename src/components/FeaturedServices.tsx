e747558a00954aa3a1aff619dfdb971bdb77e2ff
designdash03 <designdash03@gmail.com>
2026-04-08 15:36:38 +0530

    video issue

--- src/components/FeaturedServices.tsx
+++ src/components/FeaturedServices.tsx
@@ -22,7 +22,7 @@ const services: { media: MediaItem[]; title: string; subtitle: string; features:
     media: [
       { type: "video", src: "/Images/Ac-Service-Video-1.mp4" },
       { type: "video", src: "/Images/Ac-Service-Video-2.mp4" },
-      { type: "video", src: "/Images/Ac-Service-video-3.mp4" },
+      { type: "video", src: "/Images/Ac-Service-Video-3.mp4" },
     ],
     title: "AC Repair & Fix",
     subtitle: "All brands & models",
